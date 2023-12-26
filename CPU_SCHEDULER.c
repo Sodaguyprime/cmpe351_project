44ba0b9a1e5394e2e5fdfccf53a536343fa53d57
Ammar Ahmed <ammarisahmed@gmail.com>
2023-12-26 20:56:28 +0000

    Added function to countlines for array + comments

--- CPU_SCHEDULER.c
+++ CPU_SCHEDULER.c
@@ -1,18 +1,40 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//declaring the functions below
 void takedata(const char *filename);
+int countlines(const char *Filename);
 
+//main function
 int main() {
-    takedata("text.txt");
+    
+    
+    takedata("text.txt");// for testing right now
+
 
     return 0;
 }
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// user-defined functions
 void takedata(const char *Filename) {
 
     FILE *fh = fopen(Filename, "r");
-
+    int lines =  countlines("text.txt");
     if (fh !=NULL)
 {
     char c;
@@ -29,4 +51,21 @@ void takedata(const char *Filename) {
 
   fclose(fp);
   
+}
+
+int countlines(const char *Filename){
+    int count = 1 ;
+    char D;
+    FILE *fh = fopen(Filename, "r");
+
+    do
+    {
+        D = fgetc(fh);
+        if (D== '\n'){
+            count++;
+        }
+    }while (D !=EOF);
+
+    fclose(fh);
+    return count;
 }
