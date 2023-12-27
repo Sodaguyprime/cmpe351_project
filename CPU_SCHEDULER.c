2cc9a41dca3e23d210ac1cf40f5cabba8a9b1945
Ammar Ahmed <ammarisahmed@gmail.com>
2023-12-27 23:06:34 +0000

    Added a menu function + worked on reading the text file

--- CPU_SCHEDULER.c
+++ CPU_SCHEDULER.c
@@ -2,16 +2,45 @@
 #include <stdlib.h>
 
 //declaring the functions below
-void takedata(const char *filename);
-int countlines(const char *Filename);
+//void takedata(const char *filename);
+int countlines(const char *filename);
+
+
+
 
 //main function
 int main() {
     
-    
-    takedata("text.txt");// for testing right now
+    //takedata("text.txt");// for testing right now
+    int choice;
+    do{
+    printf("                         %s", "CPU SCHEDULER SIMULATOR""\n");
+    printf("1) Scheduling Method (None)\n");
+    printf("2) Preemptive Mode(off)\n");
+    printf("3) Show Result)\n");
+    printf("4) End Program\n");
 
+   
+    scanf("%d",&choice);
+    switch (choice) {
+        case 1:
+            printf("Scheduling Method\n");
+            break;
+        case 2:
+            printf("Preemptive Mode\n");
+            break;
+        case 3:
+            printf("Show Result\n");
+            break;
+        case 4:
+            printf("Exiting.......\n");
+            return 0;
+        default:
+            printf("Invalid choice\n");
+    }
 
+    }while(choice!=4);
+    int lines = (countlines("text.txt"))-1;    
     return 0;
 }
 
@@ -30,17 +59,34 @@ int main() {
 
 
 
-// user-defined functions
-void takedata(const char *Filename) {
 
-    FILE *fh = fopen(Filename, "r");
+// user-defined functions
+/* void takedata(const char *filename) {
+
+    FILE *fh = fopen(filename, "r");
     int lines =  countlines("text.txt");
     if (fh !=NULL)
 {
-    char c;
+    int c;
     while ((c= fgetc(fh)) != EOF) 
     {
-     putchar(c);
+    int Table_array[lines][3];   
+
+    for (int i=0;i<lines;i++){
+        for (int j=0;j<3;j++){
+        fscanf(fh,"%d",Table_array[i][j]);
+        }
+    }
+
+
+     for (int o = 0; o < lines; o++) {
+        for (int p = 0; p < 3; p++) {
+            printf("%d ", Table_array[o][p]);
+        }
+        printf("\n");
+    }
+
+
     }
     fclose(fh);
 
@@ -51,12 +97,14 @@ void takedata(const char *Filename) {
 
   fclose(fp);
   
-}
+} */
 
-int countlines(const char *Filename){
+
+
+int countlines(const char *filename){
     int count = 1 ;
     char D;
-    FILE *fh = fopen(Filename, "r");
+    FILE *fh = fopen(filename, "r");
 
     do
     {
